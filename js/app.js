5761376d10aa9028b24c15a58a53236effc88a90
Adrián Andrási <38466124+AdrianAndrasi@users.noreply.github.com>
2018-07-23 13:46:00 +0200

    fixed little thing

--- js/app.js
+++ js/app.js
@@ -1,71 +1,34 @@
-var min = 0;
-var sec = 0;
-var hours = 0;
-var letsStop = 0;
-window.onload = function() {
+// Create a list that holds all of your cards
+let cardsName = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+let cards = [];
+let openCards = [];
+
+// initial states
+let sec = 0,
+    min = 0,
+    moves = 0,
+    stop = 0;
+let stars = 3;
+
+// timer Self-Executing Anonymous Functions
+(function() {
     setInterval(function() {
-        if (letsStop !== 1) {
+        if (moves !== 0 && stop !== 1) {
             sec++;
+
             if (sec === 60) {
                 min++;
                 sec = 0;
             }
-            if (min === 60) {
-                hours++;
-                min = 0;
-                sec = 0;
+            $('.timer').html(`${sec}sec`);
+
+            if (min > 0) {
+                $('.timer').html(`${min}min ${sec}sec`);
             }
-            $('.timer').html(hours + ':' + min + ':' + sec);
-            // if(letsStop === 1)
-            // {
-            //     break;
-            // } 
-            console.log(min);
-            console.log(sec);
         }
-
     }, 1000);
-};
+})();
 
-/*
- * Create a list that holds all of your cards
- */
-var cards = [];
-var cardsName = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
-var openCards = [];
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-$('.deck').each(function() {
-    $(this).find('li').each(function() {
-        cards.push($(this));
-    });
-});
-var temp = 0;
-
-cardsName = shuffle(cardsName);
-
-var cardNumber = 0;
-$('.deck').each(function() {
-    $(this).find('li').find('i').each(function() {
-        $(this).removeAttr('class');
-        $(this).addClass(cardsName[cardNumber]);
-        cardNumber++;
-    });
-});
-
-//console.log($($(cards[0][0]).find('i')[0]).attr('class'));
-$('.deck').each(function() {
-    $(this).find('li').find('i').each(function() {
-        var tempClass = $($(cards[temp][0]).find('i')[0]).attr('class');
-        $(this).removeAttr('class');
-        $(this).addClass(tempClass);
-        temp++;
-    });
-});
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length,
@@ -82,107 +45,130 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-var moves = 0,
-    stars = 3;
+// disable activate fix for not clicking more than 2 cards at once
+function disable() {
+    $('.deck').css("pointer-events", "none");
+}
 
+function active() {
+    $('.deck').css("pointer-events", "auto");
+}
+
+// add each card's HTML to the page; put it into cards array
+$('.deck').each(function() {
+    $(this).find('li').each(function() {
+        cards.push($(this));
+    });
+});
+
+// shuffle the list of cards using the provided "shuffle" method
+cardsName = shuffle(cardsName);
+
+// loop through each card and create its HTML
+let cardNumber = 0;
+$('.deck').each(function() {
+    $(this).find('li').find('i').each(function() {
+        $(this).removeAttr('class');
+        $(this).addClass(cardsName[cardNumber]);
+        cardNumber++;
+    });
+});
+
+// remove properties function; hides card
 removeProperties = function(prop) {
     setTimeout(function() {
-        prop.removeClass('show open animated wobble');
-        openCards[0].removeClass('show open animated wobble');
+        prop.removeClass('show open animated pulse');
+        openCards[0].removeClass('show open animated pulse');
         openCards = [];
-    }, 400);
+    }, 600);
 };
 
+/// main onclick handler
 showCardOnClick = function(clickEvent) {
     clickEvent.on('click', function() {
+        // add moves
         moves++;
-        if (moves === 16) {
 
-        } else if (moves > 16 && moves <= 25) {
+        // remove stars after moves
+        if (moves === 16) {
+        } else if (moves > 24 && moves <= 30) {
             $('section ul li').hide();
             $('section ul').append('<li><i class="fa fa-star"></i></li>');
             $('section ul').append('<li><i class="fa fa-star"></i></li>');
             stars = 2;
-        } else if (moves > 25) {
+        } else if (moves > 30) {
             $('section ul li').hide();
             $('section ul').append('<li><i class="fa fa-star"></i></li>');
             stars = 1;
         }
+
+        // flips card when they are same; when they match
         $('.moves').html(moves);
         if ((openCards.length % 2) === 0) {
-            $(this).addClass('show open animated wobble');
+            $(this).addClass('show open animated pulse');
             $(this).off('click');
             openCards.push($(this));
         } else if (openCards.length !== 0) {
-            $(this).addClass('show open animated wobble');
+            $(this).addClass('show open animated pulse');
 
-            var self = $(this);
-            for (var i = 0; i < openCards.length; i++) {
+            // allows to flip the cards till it finds all pairs
+            let self = $(this);
+            for (let i = 0; i < openCards.length; i++) {
                 if (openCards[i].find('i').attr('class') === self.find('i').attr('class')) {
-                    // openCards.push(self);
                     self.removeClass('animated wobble');
                     self.addClass('show match animated rubberBand');
-                    openCards[i].removeClass('animated wobble');
+                    openCards[i].removeClass('animated pulse');
                     openCards[i].addClass('show match animated rubberBand');
                     console.log('match');
                     $(this).off('click');
-                    //openCards.push(self);
                     openCards = [];
+                    disable();
+                    setTimeout(active, 600);
                     break;
-                } else {
-                    self.addClass('show open animated wobble');
+                }
+                else {
+                    self.addClass('show open animated pulse');
+                    disable();
+                    setTimeout(active, 600);
                     removeProperties(self);
                     openCards[0].on('click', showCardOnClick(openCards[0]));
                     console.log('no match');
                 }
             }
         }
+
+        // sweetalert2 instead kind of modal on finding all pairs
         if ($('.deck').find('.match').length === 16) {
             setTimeout(function() {
                 $('.deck').each(function() {
-                    // $(this).find('li').hide();
                     swal({
-                        title: 'Congratulations',
+                        title: 'Congratulations! You Won!',
                         type: 'success',
-                        text: 'You have won the game . Moves conceded are ' + moves + '. You have got ' + stars + ' Stars Time taken is ' + hours + ' Hours ' + min + ' Minutes and ' + sec + ' Seconds',
+                        text: `in ${min} minute(s) and ${sec} seconds with ${moves} Moves and ${stars} Star(s) Woooooo!`,
                         allowOutsideClick: false,
                         showCancelButton: true,
                         confirmButtonText: 'Play Again',
-                        confirmButtonColor: '#0000FF',
+                        confirmButtonColor: '#02ccba',
                         cancelButtonText: 'Close',
-                        cancelButtonColor: '#FF0000'
+                        cancelButtonColor: '#6c6c6c'
                     }).then(function() {
                         location.reload();
                     }, function(dismiss) {
                         console.log('Yes');
                     });
-
                 });
             }, 300);
-            letsStop = 1;
-            $('.timer').hide();
-            $('.timer').html('0:0:0');
-            $('.timer').show();
+            stop = 1;
         }
-
-
     });
 };
 
-for (var i = 0; i < cards.length; i++) {
+// flips the cards on click
+for (let i = 0; i < cards.length; i++) {
     cards[i].on('click', showCardOnClick(cards[i]));
 }
 
-$('.restart').on('click', function() {
-    location.reload();
+// reset game
+$('.restart').click(function() {
+  location.reload(true);
 });
